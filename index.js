17001005a186bb62e6082e9882066c9d97eb1c45
Derine Banis <d.banis.13@gmail.com>
2022-06-10 12:02:21 -0700

    Code Cleanup

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ function newTask(title, desc) {
     completeTask: function() {
       this.complete = true;
     },
-    logTaskState : function () {
+    logTaskState : function() {
       console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     }
-  }
+  };
   return task;
 }
 
-const task1 = newTask('Code more', 'Finish the compass work and do stretch exercises if you have time')
-const task2 = newTask('Do groceries', 'Check the companion app for what items need to be bought and purchase them')
-const task3 = newTask('Race,race,race', 'Play some NFS: Heat')
+const task1 = newTask('Code more', 'Finish the compass work and do stretch exercises if you have time');
+const task2 = newTask('Do groceries', 'Check the companion app for what items need to be bought and purchase them');
+const task3 = newTask('Race,race,race', 'Play some NFS: Heat');
 const taskList = [task1, task2, task3];
 
-console.log(taskList)
-task3.completeTask()
-task3.logTaskState()
-task2.logTaskState()
+console.log(taskList);
+task3.completeTask();
+task3.logTaskState();
+task2.logTaskState();
