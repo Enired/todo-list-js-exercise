c851504cddd7f2959a13103fae0ce74710df4ddb
Derine Banis <d.banis.13@gmail.com>
2022-06-10 11:57:31 -0700

    Refactored to make use of Object-Oriented Programming

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-
-// Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+function newTask(title, desc) {
+  const task = {
+    title,
+    desc,
+    complete: false,
+    completeTask: function() {
+      this.complete = true;
+    },
+    logTaskState : function () {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    }
+  }
+  return task;
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
 
 // Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask('Code more', 'Finish the compass work and do stretch exercises if you have time')
+const task2 = newTask('Do groceries', 'Check the companion app for what items need to be bought and purchase them')
+const task3 = newTask('Race,race,race', 'Play some NFS: Heat')
+const taskList = [task1, task2, task3];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+console.log(taskList)
+task3.completeTask()
+task3.logTaskState()
+
+task2.logTaskState()
+
+
+// logTaskState(0); // Clean Cat Litter has not been completed
+// completeTask(0);
+// logTaskState(0); // Clean Cat Litter has been completed
