6bd13a475a39d6550005eb429a8993db7e753538
Derine Banis <d.banis.13@gmail.com>
2022-06-10 12:01:47 -0700

    Code Cleanup

--- index.js
+++ index.js
@@ -14,12 +14,6 @@ function newTask(title, desc) {
   return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-
-// Print the state of a task to the console in a nice readable way
-
-// DRIVER CODE BELOW
-
 const task1 = newTask('Code more', 'Finish the compass work and do stretch exercises if you have time')
 const task2 = newTask('Do groceries', 'Check the companion app for what items need to be bought and purchase them')
 const task3 = newTask('Race,race,race', 'Play some NFS: Heat')
@@ -28,10 +22,4 @@ const taskList = [task1, task2, task3];
 console.log(taskList)
 task3.completeTask()
 task3.logTaskState()
-
 task2.logTaskState()
-
-
-// logTaskState(0); // Clean Cat Litter has not been completed
-// completeTask(0);
-// logTaskState(0); // Clean Cat Litter has been completed
